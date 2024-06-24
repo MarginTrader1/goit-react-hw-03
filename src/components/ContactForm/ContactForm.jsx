a5ff638ed34b22953ddcbb6e2e6094baa44634b9
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2024-06-24 11:53:45 +0300

    Добавил Formik

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,29 +1,59 @@
+import { useId } from "react";
+
 import { nanoid } from "nanoid";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const initialValues = {
+  name: "",
+  number: "",
+};
+
+const ValidationSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(3, "Too Short!")
+    .max(25, "Too Long!")
+    .required("Required"),
+  number: Yup.string()
+    .min(3, "Too Short!")
+    .max(12, "Too long")
+    .required("Required"),
+});
 
 const ContactForm = ({ addContact }) => {
-   const submit = (evt) => {
-      evt.preventDefault();
+  const nameFieldId = useId();
+  const numberFieldId = useId();
 
-      const form = evt.target;
-      const { name, number } = form.elements;
-      console.log(name.value, number.value);
+  const submit = (values, actions) => {
+    addContact({
+      //добавляем контакт
+      id: nanoid(),
+      ...values,
+    });
+    actions.resetForm();
+  };
 
-      addContact({
-         id: nanoid(),
-         name: name.value,
-         number: number.value,
-      });
-
-      form.reset();
-   };
-
-   return (
-      <form onSubmit={submit}>
-         <input type="text" name="name" />
-         <input type="number" name="number" />
-         <button type="submit">{"Add Contact"}</button>
-      </form>
-   );
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={submit}
+      validationSchema={ValidationSchema}
+    >
+      <Form>
+        <div>
+          <label htmlFor={nameFieldId}>Name</label>
+          <Field type="text" name="name" id={nameFieldId} />
+          <ErrorMessage name="name" component="span" />
+        </div>
+        <div>
+          <label htmlFor={numberFieldId}>Number</label>
+          <Field type="text" name="number" id={numberFieldId} />
+          <ErrorMessage name="number" component="span" />
+        </div>
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
+  );
 };
 
 export default ContactForm;
