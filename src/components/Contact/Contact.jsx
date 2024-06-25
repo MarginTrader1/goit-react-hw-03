6ba21319f9c106ce1a7df85280ff690c577ff8b0
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2024-06-25 15:12:48 +0300

    переробив СontactList

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,30 +3,24 @@ import { FaUser } from "react-icons/fa6";
 
 import css from "./Contact.module.css";
 
-const Contact = ({ contacts, deleteContact }) => {
-   return (
-      <>
-         {contacts.map(({ id, name, number }) => {
-            return (
-               <li key={id} className={css.card}>
-                  <div className={css.contact}>
-                     <div className={css.cardName}>
-                        <FaUser />
-                        <p>{name}</p>
-                     </div>
-                     <div className={css.cardNumber}>
-                        <FaPhoneAlt />
-                        <p>{number}</p>
-                     </div>
-                  </div>
-                  <button className={css.button} onClick={() => deleteContact(id)}>
-                     {"Delete"}
-                  </button>
-               </li>
-            );
-         })}
-      </>
-   );
+const Contact = ({ id, name, number, deleteContact }) => {
+  return (
+    <>
+      <div className={css.contact}>
+        <div className={css.cardName}>
+          <FaUser />
+          <p>{name}</p>
+        </div>
+        <div className={css.cardNumber}>
+          <FaPhoneAlt />
+          <p>{number}</p>
+        </div>
+      </div>
+      <button className={css.button} onClick={() => deleteContact(id)}>
+        {"Delete"}
+      </button>
+    </>
+  );
 };
 
 export default Contact;
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,11 +3,22 @@ import Contact from "../Contact/Contact";
 import css from "./ContactList.module.css";
 
 const ContactList = ({ contacts, deleteContact }) => {
-   return (
-      <ul className={css.list}>
-         <Contact contacts={contacts} deleteContact={deleteContact} />
-      </ul>
-   );
+  return (
+    <ul className={css.list}>
+      {contacts.map(({ id, name, number }) => {
+        return (
+          <li key={id} className={css.card}>
+            <Contact
+              id={id}
+              name={name}
+              number={number}
+              deleteContact={deleteContact}
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default ContactList;
